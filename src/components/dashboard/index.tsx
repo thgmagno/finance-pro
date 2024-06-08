3edb7c9bbf6b4eeaa8969743a76042284cf193b9
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-06-08 00:55:31 -0300

    update charts

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,49 +1,89 @@
 'use client'
 
 import { Transaction } from '@/lib/types'
-import { Line } from 'react-chartjs-2'
+import { Bar } from 'react-chartjs-2'
 import {
   Chart as ChartJS,
   LinearScale,
   CategoryScale,
-  LineElement,
+  BarElement,
   PointElement,
   Legend,
   Title,
 } from 'chart.js'
+import { monthToString } from '@/utils/monthToString'
 
 ChartJS.register(
   LinearScale,
   CategoryScale,
-  LineElement,
+  BarElement,
   PointElement,
   Legend,
   Title,
 )
 
-export function Dashboard({ data }: { data: Transaction[] }) {
-  const despesasTotal = data
-    .filter((dt) => dt.type === 'EXPENSE')
-    .reduce((total, current) => total + current.amount, 0)
+type AggregatedData = {
+  [key: string]: {
+    incomes: number
+    expenses: number
+  }
+}
 
-  const receitasTotal = data
-    .filter((dt) => dt.type === 'INCOME')
-    .reduce((total, current) => total + current.amount, 0)
+export function Dashboard({ data }: { data: Transaction[] }) {
+  const aggregatedData = data.reduce((acc: AggregatedData, transaction) => {
+    const { month, year, amount, type, status } = transaction
+    // const key = `${monthToString(month).slice(0, 3)}/${year.toString().slice(2)}`
+    const key = `${year}-${month}`
+
+    if (!acc[key]) {
+      acc[key] = { incomes: 0, expenses: 0 }
+    }
+
+    if (type === 'INCOME' && status === 'OK') {
+      acc[key].incomes += amount
+    } else if (type === 'EXPENSE') {
+      acc[key].expenses += amount
+    }
+
+    return acc
+  }, {})
+
+  const labels = Object.keys(aggregatedData).sort((a, b) => {
+    const partsA = a.split('-')
+    const partsB = b.split('-')
+    const dateA = new Date(parseInt(partsA[0]), parseInt(partsA[1]), 1)
+    const dateB = new Date(parseInt(partsB[0]), parseInt(partsB[1]), 1)
+    return dateA.getTime() - dateB.getTime()
+  })
+
+  const formatedLabels = labels.map((label) => {
+    const parts = label.split('-')
+
+    return `${monthToString(parseInt(parts[1])).slice(0, 3)}/${parts[0].slice(2)}`
+  })
+
+  const incomesData = labels.map((label) => aggregatedData[label].incomes)
+  const expensesData = labels.map((label) => aggregatedData[label].expenses)
 
   const chartData = {
-    labels: ['Receitas', 'Despesas'],
+    labels: formatedLabels,
     datasets: [
       {
-        label: 'Valor',
-        data: [receitasTotal, despesasTotal],
-        backgroundColor: ['#36A2EB', '#FF6384'],
+        label: 'Receitas',
+        data: incomesData,
+        backgroundColor: '#36A2EB',
+      },
+      {
+        label: 'Despesas',
+        data: expensesData,
+        backgroundColor: '#FF6384',
       },
     ],
   }
 
   return (
-    <div className="px-10">
-      <Line data={chartData} />
+    <div className="my-5 max-w-lg rounded-lg border border-slate-400 bg-zinc-300 p-5 shadow-md">
+      <Bar data={chartData} />
     </div>
   )
 }
