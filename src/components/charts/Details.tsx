8baffae9ea9204b8e673ac7317a782aafbfc5464
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-07-04 15:29:31 -0300

    update

--- src/components/charts/Details.tsx
+++ src/components/charts/Details.tsx
@@ -187,7 +187,7 @@ export function Details({ data }: { data: Transaction[] }) {
                       href={{
                         pathname: '/despesa',
                         query: {
-                          mes: selectedDate.month,
+                          mes: selectedDate.month + 1,
                           ano: selectedDate.year,
                           categoria: category,
                         },
